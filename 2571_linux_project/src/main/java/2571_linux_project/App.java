4b92bedf5e8dc9ae7a2e89f4d477b89034ef26b0
sainath.kota <sainath.kota@gmail.com>
2022-04-25 15:31:03 +0530

    commit 2571_linux_project

---
+++ 2571_linux_project/src/main/java/2571_linux_project/App.java
@@ -0,0 +1,13 @@
+package 2571_linux_project;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
